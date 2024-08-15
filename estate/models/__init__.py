03fac17e24d87247f2bd7987267e1535c7675505
aditmulyatama <adit.mulyatama211201@gmail.com>
2024-08-15 15:47:20 +0700

    chore: Refactor estate module file names and structure

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import estate_property
-from . import estate_property_type
-from . import estate_property_tag
-from . import estate_property_offer
-from . import inherited_users
+from . import property
+from . import property_offer
+from . import property_tag
+from . import property_type
--- estate/models/models.py
+++ estate/models/models.py
@@ -1,19 +1,21 @@
-# -*- coding: utf-8 -*-
+import os
 
-# from odoo import models, fields, api
+# Get the directory path from the user
+directory_path = input("Enter the directory path: ")
 
+# Validate if the directory exists
+if os.path.exists(directory_path):
+    # Get all file names in the specified directory
+    files = os.listdir(directory_path)
 
-# class estate(models.Model):
-#     _name = 'estate.estate'
-#     _description = 'estate.estate'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    # Loop through each file
+    for filename in files:
+        # Check if 'estate_' is in the filename
+        if filename.startswith('estate_'):
+            # Create new filename by removing 'estate_'
+            new_filename = filename.replace('estate_', '', 1)
+            # Rename file to new filename
+            os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
+    print("File names updated successfully!")
+else:
+    print(f"Directory '{directory_path}' does not exist.")
