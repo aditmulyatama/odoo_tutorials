40947e3e411eb4bab82e94eee40fc9ba92ff07da
aditmulyatama <adit.mulyatama211201@gmail.com>
2024-10-22 11:33:13 +0700

    Refactor clicker service to add level and clickBots properties
    - Added level property to track the current level of the clicker game
    - Added clickBots property to represent the number of purchased robots
    - Modified increment function to level up the game based on certain thresholds
    - Added buyClickBot function to subtract click count and increase clickBots
    - Added interval to increment clicks based on the number of clickBots
    
    Fixes #123

--- awesome_clicker/static/src/js/clicker_sevice.js
+++ awesome_clicker/static/src/js/clicker_sevice.js
@@ -10,22 +10,42 @@ const clickerStateService = {
         const state = reactive({
             clickCount: 0,
             clickValue: '0.00',
-
+            level: 0, //incremented at some milestones and open new features
+            clickBots: 0, // represents the number of robot that have been purchased
         });
+
         function increment(inc) {
             state.clickCount += inc * 10;
             state.clickValue = humanNumber(state.clickCount, { decimals: 1, minDigits: 0 });
+            levelUp();
+        }
 
+        function buyClickBot(price) {
+            state.clickCount -= price;
+            state.clickValue = humanNumber(state.clickCount, { decimals: 1, minDigits: 0 });
+            state.clickBots += 1;
         }
-        function humanizeClickCount() {
-            return humanNumber(state.clickCount);
+
+        function levelUp() {
+            const thresholds = [500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000];
+            if (state.clickCount >= thresholds[state.level]) {
+                state.level += 1;
+            }
         }
+
+        // Set a 10-second interval to increment clicks by 10 * clickBots
+        setInterval(() => {
+            if (state.clickBots > 0) {
+                increment(state.clickBots * 10);
+            }
+        }, 10000);
+
         document.addEventListener("click", () => increment(1), true);
 
         return {
             state,
             increment,
-            humanizeClickCount,
+            buyClickBot,
         };
     },
 };
